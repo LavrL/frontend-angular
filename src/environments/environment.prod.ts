aa0593dc7a4f61fe602139c36c72dc4e468a0c07
Lavrentijs L <llembersk@gmail.com>
2019-09-24 13:34:42 +0300

    Updated environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyADPV4B8or0BYlxohc29_JgmgWu2X_vo4w',
+    authDomain: 'frontend-angular-734f8.firebaseapp.com',
+    databaseURL: 'https://frontend-angular-734f8.firebaseio.com',
+    projectId: 'frontend-angular-734f8',
+    storageBucket: '',
+    messagingSenderId: '122437498320',
+    appId: '1:122437498320:web:403ab474f1ea1b65'
+  }
 };
